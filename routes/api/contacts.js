9b85c85713d579c4b07e67723883355a0509f02d
Olga Birkovska <aiba.aki9@gmail.com>
2023-02-15 22:44:02 +0200

    joi validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,7 @@
 const express = require('express')
-
 const router = express.Router()
-
 const contacts = require('../../models/contacts');
+const Joi = require('joi');
 
 router.get('/', async (req, res, next) => {
   const contactList = await contacts.listContacts();
@@ -18,15 +17,20 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  if (!req.body.name) {
-    return res.status(400).json({ message: "missing required name field" });
-  } else if (!req.body.email) {
-    return res.status(400).json({ message: "missing required email field" });
-  } else if (!req.body.phone) {
-    return res.status(400).json({ message: "missing required phone field" });
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().regex(/[0-9]/).required()
+  });
+
+  try {
+      await schema.validateAsync(req.body);
+      const addNewContact = await contacts.addContact(req.body);
+      return res.status(201).json(addNewContact);
   }
-  const addNewContact = await contacts.addContact(req.body);
-  res.status(201).json(addNewContact);
+  catch (err) {
+    return res.status(400).json(err.details[0].message);
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
@@ -42,11 +46,23 @@ router.put('/:contactId', async (req, res, next) => {
     return res.status(400).json({ message: "missing fields" });
   }
 
-  const tryUpdateContact = await contacts.updateContact(req.params.contactId, req.body);
-  if (!tryUpdateContact) {
-    return res.status(404).json({ message: 'Not found' }); 
+  const schema = Joi.object({
+    name: Joi.string().optional(),
+    email: Joi.string().email().optional(),
+    phone: Joi.string().regex(/[0-9]/).optional()
+  });
+
+  try {
+      await schema.validateAsync(req.body);
+      const tryUpdateContact = await contacts.updateContact(req.params.contactId, req.body);
+      if (!tryUpdateContact) {
+        return res.status(404).json({ message: 'Not found' }); 
+      }
+      return res.status(200).json(tryUpdateContact);
+  } catch (err) {
+    return res.status(400).json(err.details[0].message);
   }
-  res.status(200).json(tryUpdateContact);
 })
+  
 
 module.exports = router
