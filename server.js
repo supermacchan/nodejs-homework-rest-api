9f4c89f6d77ee1ef8b014e5231c8b8a45e994658
Olga Birkovska <aiba.aki9@gmail.com>
2023-02-21 20:13:01 +0200

    starting with mongoose

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+// require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+console.log(process.env)
+
+const connection = mongoose.connect(uriDb, {
+  promiseLibrary: global.Promise,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log("Database connection successful");
+    });
+  })
+  .catch(err => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
