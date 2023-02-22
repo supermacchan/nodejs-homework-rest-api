921fdf8202b0578d55876a508cff268fb7b969fc
Olga Birkovska <aiba.aki9@gmail.com>
2023-02-22 22:23:02 +0200

    Update contactsService.js
    base file for services

---
+++ services/contactsService.js
@@ -0,0 +1,29 @@
+
+
+const getContacts = () => {
+
+};
+
+const getContactById = () => {
+
+};
+
+const addContact = () => {
+
+};
+
+const updateContact = () => {
+
+};
+
+const removeContact = () => {
+
+};
+
+module.exports = {
+    getContacts,
+    getContactById,
+    addContact,
+    updateContact,
+    removeContact
+}
