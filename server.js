dff43a2b031ed0a392fe925493945eaa5a047477
Olga Birkovska <aiba.aki9@gmail.com>
2023-02-22 19:57:18 +0200

    Update server.js
    "Database connection successful"

--- server.js
+++ server.js
@@ -3,28 +3,19 @@ const mongoose = require('mongoose');
 // require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const uriDb = process.env.DB_HOST;
-
-console.log(process.env)
+const uriDb = "mongodb+srv://aibaaki9:1L0veGoIT@cluster0.ixvchmc.mongodb.net/?retryWrites=true&w=majority";
 
 const connection = mongoose.connect(uriDb, {
   promiseLibrary: global.Promise,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
 connection
   .then(() => {
-    app.listen(PORT, function () {
-      console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log("Database connection successful")
     });
   })
   .catch(err => {
     console.log(`Server not running. Error message: ${err.message}`);
     process.exit(1);
   });
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
