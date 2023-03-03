b1c5676947d092fe3d348a24137ba880fab3e17c
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-03 14:55:44 +0200

    tmp directory

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,19 +3,29 @@ const router = express.Router()
 const multer  = require('multer')
 const path = require('path')
 
-const storage = multer.diskStorage({
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.resolve('./public/avatars'));
+//     },
+//     filename: (req, file, cb) => {
+//         const [fileName, extension] =  file.originalname.split('.');
+//         cb(null, `${fileName}.${extension}`);
+//     }
+// })
+
+const upload = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.resolve('./public/avatars'));
+        cb(null, path.resolve('./tmp'));
     },
     filename: (req, file, cb) => {
         const [fileName, extension] =  file.originalname.split('.');
         cb(null, `${fileName}.${extension}`);
-        // добавить пак uuid
-        // в данном примере - оригинальное имя файла
     }
 })
 
-const uploadMiddleware = multer({storage});
+const initialUploadMiddleware = multer({upload});
+// const uploadMiddleware = multer({storage});
+
 const { 
     authMiddleware,
     credentialsCheckMiddleware
@@ -37,7 +47,7 @@ router.post('/login', credentialsCheckMiddleware, loginController);
 router.get('/current', authMiddleware, checkCurrentUserController);
 router.get('/logout', authMiddleware, logoutController);
 router.patch('/', authMiddleware, subscriptionCheckMiddleware, updateSubscriptionController);
-router.patch('/avatars', authMiddleware, uploadMiddleware.single('avatar'), avatarUploadController);
+router.patch('/avatars', authMiddleware, initialUploadMiddleware.single('avatar'), avatarUploadController);
  
 
 module.exports = { authRouter: router };
